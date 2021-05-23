134658d99013727970cc4bb644caf27a819f895e
GonzaloHub <gonzalo.2306@hotmail.com>
2021-05-23 16:53:34 -0500

    Update, Delete y Busqueda por dni

--- src/main/java/com/empresa/controller/AlumnoController.java
+++ src/main/java/com/empresa/controller/AlumnoController.java
@@ -1,11 +1,17 @@
 package com.empresa.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,5 +47,49 @@ public class AlumnoController {
 			return ResponseEntity.badRequest().build();
 		}
 
+	}
+
+	@PutMapping
+	public ResponseEntity<Alumno> actualiza(@RequestBody Alumno obj) {
+		log.info(">>>>>>alumno id>>>>>" + obj.getIdAlumno());
+		Optional<Alumno> optAlumno = service.obtienePorId(obj.getIdAlumno());
+		if (optAlumno.isPresent()) {
+			Alumno objSalida = service.insertaActualizaAlumno(obj);
+			if (objSalida != null) {
+				return ResponseEntity.ok(objSalida);
+			} else {
+				return ResponseEntity.badRequest().build();
+			}
+		} else {
+			log.info(">>>> actualiza - no existe el id : " + obj.getIdAlumno());
+			return ResponseEntity.badRequest().build();
+		}
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Alumno> elimina(@PathVariable int id) {
+		log.info(">>>> elimina el id : " + id);
+		Optional<Alumno> optAlumno = service.obtienePorId(id);
+		if (optAlumno.isPresent()) {
+			service.eliminaAlumno(id);
+			return ResponseEntity.ok(optAlumno.get());
+
+		} else {
+			log.info(">>>> elimina - no existe el id : " + id);
+			return ResponseEntity.badRequest().build();
+		}
+	}
+
+	@GetMapping("/buscarPorDNI/{dni}")
+	public ResponseEntity<List<Alumno>> buscar(@PathVariable("dni") String dni) {
+		log.info(">>>> busca por dni : " + dni);
+		List<Alumno> lstAlumno = service.listaPorDni(dni);
+		if (!CollectionUtils.isEmpty(lstAlumno)) {
+			return ResponseEntity.ok(lstAlumno);
+
+		} else {
+			log.info(">>>> buscar por dni - no existen alumnos con ese dni : " + dni);
+			return ResponseEntity.badRequest().build();
+		}
 	}
 }
